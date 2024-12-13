11fed044069a171d05fe4131d572dedc20389de3
EvilHui <Ukrumoski@gmail.com>
2024-12-13 21:46:01 +0500

    такс 3

---
+++ Tyuiu.KimmelDS.Sprint5.Task3.V21.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KimmelDS.Sprint5.Task3.V21.Lib
+{
+    public class DataService : ISprint5Task3V21
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double result = (Math.Pow(x, 2) + 1) / Math.Sqrt(4 * Math.Pow(x, 3) - 3);
+
+            result = Math.Round(result, 3);
+
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            Console.WriteLine($"Вычисленное значение: {result}");
+            Console.WriteLine($"Файл сохранен по пути: {filePath}");
+
+            return filePath;
+        }
+    }
+}
+}
---
+++ Tyuiu.KimmelDS.Sprint5.Task3.V21/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint5.Task3.V21.Lib;
+namespace Tyuiu.KimmelDS.Sprint5.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.SaveToFileTextData(5);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
