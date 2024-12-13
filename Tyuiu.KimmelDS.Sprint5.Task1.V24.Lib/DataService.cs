f48a7dd22569f254732b5441f0c6f173482427cf
EvilHui <Ukrumoski@gmail.com>
2024-12-13 20:53:31 +0500

    task1

---
+++ Tyuiu.KimmelDS.Sprint5.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,55 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KimmelDS.Sprint5.Task1.V24.Lib
+{
+    public class DataService : ISprint5Task1V24
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string filePath = "OutPutFileTask1.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                Console.WriteLine("x\tF(x)");
+                writer.WriteLine("x\tF(x)");
+
+                for (double x = startValue; x <= stopValue; x += 1)
+                {
+                    double result = CalculateFunction(x);
+
+                    result = Math.Round(result, 2);
+
+                    Console.WriteLine($"{x}\t{result}");
+                    writer.WriteLine($"{x}\t{result}");
+                }
+            }
+            return filePath;
+        }
+
+        static double CalculateFunction(double x)
+        {
+            try
+            {
+                double denominator = (4 * x - 0.5);
+                if (denominator == 0)
+                {
+                    return 0; 
+                }
+
+                // Основная функция
+                double result = (3 * Math.Cos(x) / denominator) + Math.Sin(x) - 5 * x - 2;
+
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                return 0; 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint5.Task1.V24/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint5.Task1.V24.Lib;
+namespace Tyuiu.KimmelDS.Sprint5.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
