751016785b1e6e52010557e5f0d294a06b6bd3d8
EvilHui <Ukrumoski@gmail.com>
2024-12-13 20:18:39 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KimmelDS.Sprint5.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KimmelDS.Sprint5.Task0.V27.Lib
+{
+    public class DataService : ISprint5Task0V27
+    {
+        public string SaveToFileTextData(int x)
+        {
+            var path = $"{Directory.GetCurrentDirectory()}\\OutPutFileTask0.txt";
+            var res = Math.Round(Math.Pow(x - 1, 3 * x + 1), 3);
+            File.WriteAllText(path, res.ToString());
+
+            return path;
+        }
+    }
+}
