88759b86f9c85bfc7878051cf49303793bd2ad3e
EvilHui <Ukrumoski@gmail.com>
2024-12-14 09:19:38 +0500

    tsak6

---
+++ Tyuiu.KimmelDS.Sprint5.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KimmelDS.Sprint5.Task6.V20.Lib
+{
+    public class DataService : ISprint5Task6V20
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int sixLetterWordCount = 0;
+
+            // Чтение данных из файла
+            using (var reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Разделяем строку на слова по пробелам и другим разделителям
+                    var words = line.Split(new char[] { ' ', ',', '.', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Считаем слова длиной ровно 6 символов
+                    sixLetterWordCount += words.Count(word => word.Length == 6);
+                }
+            }
+
+            // Возвращаем результат
+            return sixLetterWordCount;
+        }
+    }
+}
