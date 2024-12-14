23385e41edbb3e0fbe089f6ab60a4aa5b19e3500
EvilHui <Ukrumoski@gmail.com>
2024-12-14 09:36:40 +0500

    task7

---
+++ Tyuiu.KimmelDS.Sprint5.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KimmelDS.Sprint5.Task7.V18.Lib
+{
+    public class DataService : ISprint5Task7V18
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string inputData;
+            using (var reader = new StreamReader(path))
+            {
+                inputData = reader.ReadToEnd();
+            }
+
+            string outputData = inputData.Replace("н", "нн");
+
+            
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (var writer = new StreamWriter(fs))
+                {
+                    writer.Write(outputData);
+                }
+            }
+
+            return path;
+        }
+    }
+}
